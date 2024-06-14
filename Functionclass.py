005307454fdba0e1cc36932337af5bf0b1e30f4e
unknown <bashmusah2014@gmail.com>
2024-06-13 22:01:50 -0700

    class

---
+++ Functionclass.py
@@ -0,0 +1,27 @@
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# for num in range (6):
+#     print(num)
+# for num in range(3,37,3):
+#     print(num)
+#     print(num*4)
+
+# for num in range(1,13):
+#     print(num*4)
+# name_list=['aisha','ada','shola','tayo']
+# for name in name_list:
+# number=[1,2,4,5]
+# for num in number:
+# print num
+LG_in_kwara=['ilorin west','ekiti','oke-ero','ifelodun','barute','asa']
+for i in range(len(LG_in_kwara)):
+    print(LG_in_kwara[i][-1])
+
+
+for lg in LG_in_kwara:
+    print(lg[-1])
---
+++ loop function.py
@@ -0,0 +1,46 @@
+# fruit=['apple','banana','cherry'] #looping through list
+# for x in fruit:
+#     print(x)
+# for x in 'banana': #looping through string
+#     print (x)
+# fruit=['apple','banana','cherry']
+# for x in fruit:
+#     print(x)
+#     if x=='banana':
+#         break
+# fruit=['apple','banana','cherry']# break statement
+# for x in fruit:
+#     if x=='banana':
+#         break
+#     print(x)
+# fruit=['apple','banana','cherry']#continue statement
+# for x in fruit:
+#     if x=='banana':
+#         continue
+#     print(x)
+# name=('ada','segun','dayo')
+# for x in name:
+#     print (x)
+# for x in name [-1]:
+#         print (x)
+# for x in range (6):
+#       print(x)
+# for x in range (1,10):
+#       print(x)
+# for x in range (2,14,7):
+#       print(x*4)
+# for x in range (2,10):
+#       print (x)
+# else:
+#       print('Mashallah')
+# for x in range (1,13,3):
+#     if x==9:break
+#     print(x)
+# else:
+#     print ('good')
+  
+for x in [0,1,2]:
+    pass
+
+    
+            
